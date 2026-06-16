a694c194fea387469bc63f489c91b00c398a109b
TannerGabriel <gabrieltanner.code@gmail.com>
2026-06-16 13:32:21 +0200

    Fix map generation

--- turtlebot3_gazebo/launch/turtlebot3_house.launch.py
+++ turtlebot3_gazebo/launch/turtlebot3_house.launch.py
@@ -37,7 +37,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
-        'turtlebot3_house.world'
+        'campus_first_floor.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
